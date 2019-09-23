cb696ee444857a8d1d0e269ae6f6aa9505618f40
Sam Forrester <SForrester@spartaglobal.com>
2019-09-23 16:02:52 +0100

    Added a new exercise, but the first part needs retuning as I did a dictionary not a list!

--- 106_Data_Type_Distinct.py
+++ 106_Data_Type_Distinct.py
@@ -13,30 +13,43 @@
 #       'example_key': 'value',
 #       'example_key': 'value'
 #       }
+#
+# # Defining a simply dictionary with two keys
+# dictionary_crazy_exes = {
+#     'Carolina': 'she was actually nice',
+#     'Arthur': 'bit of a drinker'
+# }
+# print(dictionary_crazy_exes)
+# print(type(dictionary_crazy_exes))
+#
+# # Accessing values - use the key!
+# print(dictionary_crazy_exes['Carolina'])
+# print(dictionary_crazy_exes['Arthur'])
+#
+# # Adding a new key, pair value
+# dictionary_crazy_exes['Kyle'] = 'Likes Monster' # Adding a new key and its value automatically adds it to the dictionary
+# print(dictionary_crazy_exes)
+#
+# # Re-assigning a value
+# dictionary_crazy_exes['Kyle'] = 'Absolutely Loves Monster' # Referencing the original key when it already exists will re-assign a value
+# print(dictionary_crazy_exes)
+#
+# # Get out all the keys
+# print(dictionary_crazy_exes.keys())
+#
+# # Get out all of the values
+# print(dictionary_crazy_exes.values())
+#
+# # Remove item from dictionary
+# dictionary_crazy_exes.pop('Arthur') #remove entries with Key of 'Arthur'
+# print(dictionary_crazy_exes)
 
-# Defining a simply dictionary with two keys
-dictionary_crazy_exes = {
-    'Carolina': 'she was actually nice',
-    'Arthur': 'bit of a drinker'
+
+# Better example of a dictionary
+
+Crazy1 = {
+    'Name': 'Carolina',
+    'Phone': '07842715517',
+    'Address': 'Location 1, at places',
+    'Known places to avoid': ['Milan', 'Lisbon', 'Tavira']
 }
-print(dictionary_crazy_exes)
-print(type(dictionary_crazy_exes))
-
-# Accessing values - use the key!
-print(dictionary_crazy_exes['Carolina'])
-print(dictionary_crazy_exes['Arthur'])
-
-# Adding a new key, pair value
-dictionary_crazy_exes['Kyle'] = 'Likes Monster' # Adding a new key and its value automatically adds it to the dictionary
-print(dictionary_crazy_exes)
-
-# Re-assigning a value
-dictionary_crazy_exes['Kyle'] = 'Absolutely Loves Monster' # Referencing the original key when it already exists will re-assign a value
-print(dictionary_crazy_exes)
-
-# Get out all the keys
-print(dictionary_crazy_exes.keys())
-
-# Get out all of the values
-print(dictionary_crazy_exes.values())
-
---
+++ Exercise/Exercise 3.py
@@ -0,0 +1,56 @@
+# # Exercise 3
+#
+# Bucket_List_Dictionary = {
+#     'Number 1': 'Climb Kilimanjaro',
+#     'Number 2': 'Box a kangaroo',
+#     'Number 3': 'Become a pokemon master',
+#     'Number 4': 'Infiltrate a herd of gazelle',
+#     'Number 5': 'Build a bridge across the atlantic ocean',
+#     'Number 6': 'Save the whales',
+#     'Number 7': 'Learn the lyrics to "We didn''t start the Fire',
+#     'Number 8': 'Prove that the earth is flat',
+#     'Number 9': 'Ride a 100-year-old Sea Turtle down a waterfall',
+#     'Number 10': 'Solve the world''s energy crisis by burning rubbish'
+# }
+#
+# print(Bucket_List_Dictionary)
+#
+# print(len(Bucket_List_Dictionary))
+#
+# print(Bucket_List_Dictionary['Number 1'])
+#
+# print(Bucket_List_Dictionary['Number 2'])
+#
+# Bucket_List_Dictionary['Number 11']='Experience true enlightenment'
+# print(Bucket_List_Dictionary)
+#
+# print(Bucket_List_Dictionary['Number 11'])
+
+# Re assign item and print all
+#
+# Exercise 3.2
+# 1
+Dictionary_Story_Time = {
+    'Start': 'Once upon a time there was a girl named Goldilocks.',
+    'Middle': 'She entered a family of bears\' house.',
+    'End': 'So they ate her... The End.'
+}
+# 2
+print(Dictionary_Story_Time)
+
+# 3
+print(type(Dictionary_Story_Time))
+
+# 4
+print(Dictionary_Story_Time.keys())
+
+print(Dictionary_Story_Time.values())
+
+# 5
+print(Dictionary_Story_Time['Start'], Dictionary_Story_Time['Middle'], Dictionary_Story_Time['End'])
+
+# 6
+Dictionary_Story_Time['Hero']='The Bears'
+print(Dictionary_Story_Time)
+
+
