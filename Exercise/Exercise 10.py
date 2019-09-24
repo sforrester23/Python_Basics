4f78159bdbb409fb0e3fd691bf8e320335824431
Sam Forrester <SForrester@spartaglobal.com>
2019-09-24 21:44:50 +0100

    Completed homework exercises 9 & 10, as well as touching up exercise 8 so it can do exactly what was desired

--- Exercise/Exercise 10.py
+++ Exercise/Exercise 10.py
@@ -13,16 +13,95 @@
 
 # extra:
     # Create a while loop so we don't stop listening to stories unless we tell it to break!
-
-story_1 =
-
 book = {
     'story_1' : {},
     'story_2' : {},
     'story_3' : {}
 }
 
-print(book)
+print('We\'re going to make a book! It\'s going to be made of three separate stories. Let\'s get started...')
 
+story = {
+    'hero' : '',
+    'villain': '',
+    'beginning' : '',
+    'middle' : '',
+    'end' : '',
+    'moral' : ''
+}
 
-book['story_1']
+for key1 in book:
+    print('Let\'s make a {} for the book!'.format(key1))
+    for key2 in story:
+        story[key2] = input('What is the {} of the story? '.format(key2)).capitalize()
+        book[key1][key2] = story[key2]
+
+# print(book)
+# print('So, this is the story we\'ve just made:')
+# print('{}. {}. {}.'.format(story_1['beginning'], story_1['middle'], story_1['end']))
+# print('The hero of the story was {}, while the Villain was {}.'.format(story_1['hero'], story_1['villain']))
+# print('The moral of the story is: {}. And don\'t you forget it...'.format(story_1['moral']))
+#
+# print('Right, now that\'s one story over, let\'s do another!')
+#
+# story = {
+#     'hero' : '',
+#     'villain': '',
+#     'beginning' : '',
+#     'middle' : '',
+#     'end' : '',
+#     'moral' : ''
+# }
+#
+# for key in story:
+#     story[key] = input('What is the {} of the story? '.format(key)).capitalize()
+#     story_2 = story
+#
+# print('So, this is the story we\'ve just made:')
+# print('{}. {}. {}.'.format(story_2['beginning'], story_2['middle'], story_2['end']))
+# print('The hero of the story was {}, while the Villain was {}.'.format(story_2['hero'], story_2['villain']))
+# print('The moral of the story is: {}. And don\'t you forget it...'.format(story_2['moral']))
+#
+# print('Finally, let\'s do one more story. Last one I promise...')
+#
+# story = {
+#     'hero' : '',
+#     'villain': '',
+#     'beginning' : '',
+#     'middle' : '',
+#     'end' : '',
+#     'moral' : ''
+# }
+#
+# for key in story:
+#     story[key] = input('What is the {} of the story? '.format(key)).capitalize()
+#     story_3 = story
+#
+# print('So, this is the story we\'ve just made:')
+# print('{}. {}. {}.'.format(story_3['beginning'], story_3['middle'], story_3['end']))
+# print('The hero of the story was {}, while the Villain was {}.'.format(story_3['hero'], story_3['villain']))
+# print('The moral of the story is: {}. And don\'t you forget it...'.format(story_3['moral']))
+#
+print('Finally, that\'s all the stories made.')
+keep_going = input('Would you like to read some back? (Y/N) ').upper()
+
+while keep_going != 'N':
+    story_to_read = int(input('Which story would you like to read back? Please enter either 1, 2 or 3: '))
+    if story_to_read == 1:
+        print('This is story number 1')
+        print('{}. {}. {}.'.format(book['story_1']['beginning'], book['story_1']['middle'], book['story_1']['end']))
+        print('The hero of the story was {}, while the Villain was {}.'.format(book['story_1']['hero'], book['story_1']['villain']))
+        print('The moral of the story is: {}. And don\'t you forget it...'.format(book['story_1']['moral']))
+    elif story_to_read == 2:
+        print('This is story number 1')
+        print('{}. {}. {}.'.format(book['story_2']['beginning'], book['story_2']['middle'], book['story_2']['end']))
+        print('The hero of the story was {}, while the Villain was {}.'.format(book['story_2']['hero'], book['story_2']['villain']))
+        print('The moral of the story is: {}. And don\'t you forget it...'.format(book['story_2']['moral']))
+    elif story_to_read == 3:
+        print('This is story number 1')
+        print('{}. {}. {}.'.format(book['story_3']['beginning'], book['story_3']['middle'], book['story_3']['end']))
+        print('The hero of the story was {}, while the Villain was {}.'.format(book['story_3']['hero'], book['story_3']['villain']))
+        print('The moral of the story is: {}. And don\'t you forget it...'.format(book['story_3']['moral']))
+    else:
+        print('Sorry, I could not decipher which story you\'d like to read').upper()
+    keep_going = input('Would you like to read some back? (Y/N) ')
--- Exercise/Exercise 8.py
+++ Exercise/Exercise 8.py
@@ -5,16 +5,26 @@
 # Multiples of 3 AND 5 are POPTOC
 
 ## Show count up to the number inputted
-
-while True:
+play_on = input('Press ENTER to play the game, type exit to leave: ').lower()
+while play_on != 'exit':
+    count = 0
     number = int(input('Please insert a number: '))
-    if number%3 == 0:
-        if number%5 == 0:
-            print('That number is POPTOC!')
+    while count < number:
+        count += 1
+        if count%3 == 0:
+            if count%5 == 0:
+                print(count)
+                print('That number is POPTOC!')
+            else:
+                print(count)
+                print('That number is POP!')
+        elif count%5 == 0:
+            print(count)
+            print('That number is TOC!')
         else:
-            print('That number is POP!')
-    elif number%5 == 0:
-        print('That number is TOC!')
-    else:
-        print('This number is neither POP nor TOC...')
-        break
+            print(count)
+            print('This number is neither POP nor TOC...')
+    play_on = input('Press ENTER to play the game again, type exit to leave: ').lower()
+
+
+
